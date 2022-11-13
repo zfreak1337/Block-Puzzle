acfd6de1d1263bce0b57de5bbec4e6257f7508eb
zfreak1337 <40974103+zfreak1337@users.noreply.github.com>
2022-11-14 05:42:01 +0600

    Add files via upload

---
+++ Settings.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour 
+{
+	public void OnRateButtonPressed()
+	{
+		if (InputManager.Instance.canInput ()) {
+			AudioManager.Instance.PlayButtonClickSound ();
+			Application.OpenURL("https://play.google.com/store/apps/details?id=com.zfreak671games.puzzle");
+		}
+	}
+
+	public void OnCloseButtonPressed()
+	{
+		if (InputManager.Instance.canInput ()) {
+			AudioManager.Instance.PlayButtonClickSound ();
+			gameObject.Deactivate();
+		}
+	}
+
+	public void OnSelectLanguageButtonPressed(){
+		if (InputManager.Instance.canInput ()) {
+			StackManager.Instance.selectLanguageScreen.Activate();
+			gameObject.Deactivate();
+		}
+	}
+}
